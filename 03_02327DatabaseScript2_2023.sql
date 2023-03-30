0ab16a95ba801915c5b39caa69689232c7776159
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2023-03-30 13:51:39 +0200

    Update 03_02327DatabaseScript2_2023.sql

---
+++ 03_02327DatabaseScript2_2023.sql
@@ -0,0 +1,6 @@
+DELETE from Journalist WHERE Address="1":
+UPDATE Journalist
+SET Email = "Testmail@gmail.com"
+WHERE First_Name = "Olga":
+INSERT Journalist VALUES(90102037, 'Muc', 'Milla', '18', '64564345', 'mosttest@hotmail.com')
+ 
